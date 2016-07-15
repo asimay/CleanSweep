ca562b03f49f0fe8a79ed1db18db4a990ed0b3ef
TheOriginalBDM <mike@terrapal.com>
2016-07-14 21:38:57 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="cleansweep",
+    version="0.0.1",
+    description="trash pickup bot",
+    py_modules=["cleansweep"],
+    install_requires=["OpenCV"]
+)
